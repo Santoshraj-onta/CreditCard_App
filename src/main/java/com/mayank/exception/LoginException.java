272b75545b241d5c731aa1892970b8b919c75266
Santoshraj-onta <santoshrajontafs@gmail.com>
2025-07-14 17:39:02 -0400

    Update LoginException

--- src/main/java/com/mayank/exception/LoginException.java
+++ src/main/java/com/mayank/exception/LoginException.java
@@ -1,14 +1,1 @@
-package com.mayank.exception;
 
-public class LoginException extends Exception {
-
-	public LoginException() {
-		
-	}
-
-	public LoginException(String msg) {
-		super(msg);
-		
-	}
-
-}
